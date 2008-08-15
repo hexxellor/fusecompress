709375ca46e6e7cac8b6ee013c071942d32d8f3b
ulrich.hecht <ulrich.hecht@36aca67c-b753-0410-8cf6-e3b166235498>
2008-08-15 13:16:22 +0000

    added my copright where appropriate clarified GPL version (some files said any, others said v2)

--- direct_compress.c
+++ direct_compress.c
@@ -2,11 +2,11 @@
  *
  * Copyright (C) 2005 Anders Aagaard <aagaande@gmail.com>
  * Copyright (C) 2006 Milan Svoboda <milan.svoboda@centrum.cz>
+ * Copyright (C) 2008 Ulrich Hecht <uli@suse.de>
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public
- * License as published by the Free Software Foundation; either
- * version 2 of the License, or (at your option) any later version.
+ * License version 2 as published by the Free Software Foundation.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
