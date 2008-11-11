0b872c42d145c9898b581b1b4eb1c89341daaec9
ulrich.hecht <ulrich.hecht@36aca67c-b753-0410-8cf6-e3b166235498>
2008-11-11 18:11:51 +0000

    fix stale cache problem properly (issue #32), add test LZO is non-deterministic, do not check compressed data in tc.sh

--- trunk/test/tc.sh
+++ trunk/test/tc.sh
@@ -6,7 +6,7 @@ do
 	gcc -g -I.. -DMODULE=$i -o tc tc.c ../compress_*.c ../file.c ../minilzo/lzo.c ../globals.c -llzma -lbz2 -lz -llzo2
 	./tc
 	cmp in back_in
-	cmp out back_out
+	test "$i" = "module_lzo" || cmp out back_out
 	cmp in and_in_again
 	rm back_in out back_out and_in_again
 done
