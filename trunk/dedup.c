b0161b35887e269136c0a8983289c5f35551b6cb
ulrich.hecht@gmail.com <ulrich.hecht@gmail.com@36aca67c-b753-0410-8cf6-e3b166235498>
2011-10-31 16:29:53 +0000

    dedup: more verbose comments
    Signed-off-by: Ulrich Hecht <uli@suse.de>

--- trunk/dedup.c
+++ trunk/dedup.c
@@ -28,19 +28,35 @@
 #include <mhash.h>
 #include <fcntl.h>
 
+/** Hard-link filename to a file from the dedup database with the same MD5
+ * hash.
+ * @param md5 file content's 128-bit MD5 hash
+ * @param filename file name
+ */
 void hardlink_file(unsigned char *md5, const char *filename)
 {
   DEBUG_("looking for '%s' in md5 database", filename);
+
+  /* search for entry with matching MD5 hash */
   LOCK(&dedup_database.lock);
   dedup_t* dp;
   list_for_each_entry(dp, &dedup_database.head, list) {
     if (memcmp(md5, dp->md5, 16) == 0) {
+      /* Check if this entry points to the file itself. */
+      /* XXX: This is something that should not actually happen, although
+         should at worst be a performance problem. */
       if(strcmp(filename, dp->filename) == 0) {
         DEBUG_("second run for '%s', ignoring", filename);
         UNLOCK(&dedup_database.lock);
         return;
       }
+
       DEBUG_("duping it up with the '%s' man", dp->filename);
+
+      /* We cannot just unlink the duplicate file because some filesystems
+         (Btrfs, NTFS) have severe limits on the number of hardlinks per
+         directory or per inode; we therefore rename it first and only delete
+         it once the link has actually been created. */
       char *tmpname = malloc(strlen(filename) + 12);
       sprintf(tmpname, "%s.%d", filename, getpid());
       if (rename(filename, tmpname)) {
@@ -49,6 +65,8 @@ void hardlink_file(unsigned char *md5, const char *filename)
         UNLOCK(&dedup_database.lock);
         return;
       }
+
+      /* Try to create the link. */
       if (link(dp->filename, filename)) {
         DEBUG_("creating hardlink from '%s' to '%s' failed", dp->filename, filename);
         if (rename(tmpname, filename)) {
@@ -56,6 +74,7 @@ void hardlink_file(unsigned char *md5, const char *filename)
         }
       }
       else {
+        /* Made it, now we can actually unlink the duplicate. */
         if (unlink(tmpname)) {
           ERR_("failed to unlink original file at '%s'", tmpname);
         }
@@ -65,6 +84,9 @@ void hardlink_file(unsigned char *md5, const char *filename)
       return;
     }
   }
+  
+  /* If we reach this point, we haven't found any duplicate for this file,
+     so we add it as a new entry into the dedup database. */
   DEBUG_("unique file '%s', adding to dedup DB", filename);
   dp = (dedup_t *)malloc(sizeof(dedup_t));
   memcpy(dp->md5, md5, 16);
@@ -76,12 +98,19 @@ void hardlink_file(unsigned char *md5, const char *filename)
   UNLOCK(&dedup_database.lock);
 }
 
+/** Attempt deduplication of file.
+ * @param file File to be deduplicated.
+ */
 void do_dedup(file_t *file)
 {
   NEED_LOCK(&file->lock);
   
   file->status |= DEDUPING;
+  /* No need to keep the file under lock while calculating the hash; should
+     it change in the meantime, we will be informed through file->status. */
   UNLOCK(&file->lock);
+  
+  /* Calculate MD5 hash. */
   MHASH mh = mhash_init(MHASH_MD5);
   int fd = open(file->filename, O_RDONLY);
   int count;
@@ -97,23 +126,30 @@ void do_dedup(file_t *file)
     if (count == 0)
       break;
     mhash(mh, buf, count);
+    /* XXX: It would be good for performance to occasionally check
+       file->status & CANCEL. */
   }
   unsigned char md5[16];
   mhash_deinit(mh, md5);
   close(fd);
+  
+  /* See if everything went fine. */
   LOCK(&file->lock);
   if (failed) {
     DEBUG_("deduping '%s' failed", file->filename);
+    /* While this looks suspicious, it is not a deduplication error, so we
+       don't have to do anything special. */
     return;
   }
   if (!(file->status & CANCEL)) {
-    /* link to identical file */
+    /* No failure, no cancellation; let's link to identical file from dedup DB. */
     DEBUG_("MD5 for '%s': %02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x",
            file->filename, md5[0], md5[1], md5[2], md5[3], md5[4], md5[5], md5[6], md5[7], md5[8],
            md5[9], md5[10], md5[11], md5[12], md5[13], md5[14], md5[15]);
     hardlink_file(md5, file->filename);
   }
   else {
+    /* Acknowledge the cancellation. */
     DEBUG_("deduping '%s' cancelled", file->filename);
     file->status &= ~CANCEL;
     pthread_cond_broadcast(&file->cond);
@@ -121,21 +157,27 @@ void do_dedup(file_t *file)
   file->status &= ~DEDUPING;
 }
 
+/** Reverse deduplication in case a hardlinked file is written to.
+ * @param file File to be undeduplicated.
+ */
 int do_undedup(file_t *file)
 {
   NEED_LOCK(&file->lock);
   DEBUG_("undeduping '%s'", file->filename);
+  
+  /* First, check if there is actually something to be done. */
   struct stat st;
   if (stat(file->filename, &st) < 0)
     return 0;
   if (st.st_nlink < 2)
     return 0;
   
-  errno = EIO;
-  
+  /* Check if we have enough space on the backing store to undedup. */
   struct statvfs stat;
-  if(statvfs(file->filename, &stat) < 0)
+  if(statvfs(file->filename, &stat) < 0) {
+          errno = EIO;
           return FAIL;
+  }
   if(stat.f_bsize * stat.f_bavail < st.st_size) {
           if(!(geteuid() == 0 && stat.f_bsize * stat.f_bfree >= st.st_size)) {
                   errno = ENOSPC;
@@ -143,22 +185,25 @@ int do_undedup(file_t *file)
           }
   }
   
+  /* XXX: Is this actually necessary? After all, we create an identical
+     copy. */
   dedup_discard(file);
   
+  /* Behold the shitload of code it takes to safely copy a file on Unix... */
   int fd_out;
   char *temp = file_create_temp(&fd_out);
   if (fd_out == -1) {
     ERR_("undedup: failed to create temp file");
+    errno = EIO;
     return FAIL;
   }
-  DEBUG_("undedup: temp ptr %p", temp);
-  DEBUG_("undedup: opened temp file '%s'", temp);
   int fd_in = open(file->filename, O_RDONLY);
   if (fd_in == -1) {
     ERR_("undedup: failed to open input file '%s'", file->filename);
     close(fd_out);
     unlink(temp);
     free(temp);
+    errno = EIO;
     return FAIL;
   }
   char buf[4096];
@@ -170,9 +215,10 @@ int do_undedup(file_t *file)
       close(fd_out);
       unlink(temp);
       free(temp);
+      errno = EIO;
       return FAIL;
     }
-    else if (count == 0)
+    else if (count == 0) /* EOF */
       break;
     if (write(fd_out, buf, count) != count) {
       ERR_("undedup: write() failed on output file '%s'", temp);
@@ -180,36 +226,48 @@ int do_undedup(file_t *file)
       close(fd_out);
       unlink(temp);
       free(temp);
+      errno = EIO;
       return FAIL;
     }
   }
   close(fd_in);
+  /* fix owner and mode of the new file */
   fchown(fd_out, st.st_uid, st.st_gid);
   fchmod(fd_out, st.st_mode);
   close(fd_out);
+  
+  /* Remove deduplicated link... */
   if (unlink(file->filename) < 0) {
     ERR_("undedup: failed to unlink '%s'", file->filename);
     unlink(temp);
     free(temp);
+    errno = EIO;
     return FAIL;
   }
+  /* ...and replace it with the new copy. */
   DEBUG_("renaming '%s' to '%s'", temp, file->filename);
   if (rename(temp, file->filename) < 0) {
     ERR_("undedup: failed to rename temp file '%s' to '%s'", temp, file->filename);
     free(temp);
+    errno = EIO;
     return FAIL;
   }
+  /* fix timestamps */
   struct utimbuf utbuf;
   utbuf.actime = st.st_atime;
   utbuf.modtime = st.st_mtime;
   if (utime(file->filename, &utbuf) < 0) {
     CRIT_("utime failed on '%s'!", file->filename);
+    errno = EIO;
     return FAIL;
   }
   errno = 0;
   return 0;
 }
 
+/** Remove an entry from the deduplication DB.
+ * @param file File to be removed.
+ */
 void dedup_discard(file_t *file)
 {
   NEED_LOCK(&file->lock);
