110c840a62d3d4dac3766d607de55153da001c9d
ulrich.hecht@gmail.com <ulrich.hecht@gmail.com@36aca67c-b753-0410-8cf6-e3b166235498>
2011-10-31 16:27:05 +0000

    make sure LZO compression is deterministic when deduping (with test)
    Signed-off-by: Ulrich Hecht <uli@suse.de>

---
+++ trunk/test/dedup_lzo_determ.py
@@ -0,0 +1,22 @@
+# verify that LZO compression is deterministic when using dedup
+
+import os
+import shutil
+import sys
+
+filecount = 100
+
+os.mkdir('test')
+if os.system('../fusecompress -o dedup,lzo,detach test') != 0:
+  os.rmdir('test')
+  sys.exit(2)	# dedup not available
+
+for i in range(0, filecount):
+  shutil.copy('/bin/sh', 'test/sh' + str(i))
+os.system('fusermount -u test')
+sh_data = open('test/sh0').read()
+for i in range(1, filecount):
+  if sh_data != open('test/sh' + str(i)).read():
+    os.abort()
+shutil.rmtree('test')
+sys.exit(0)
