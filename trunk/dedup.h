8c32b1795a99806550b394c0b3ea298c4791e176
ulrich.hecht <ulrich.hecht@36aca67c-b753-0410-8cf6-e3b166235498>
2011-11-04 13:27:58 +0000

    dedup: factor out some code to separate functions so it can be used by fsck
    Signed-off-by: Ulrich Hecht <uli@suse.de>

--- trunk/dedup.h
+++ trunk/dedup.h
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "structs.h"
 
 void do_dedup(file_t *file);
@@ -7,3 +8,8 @@ void dedup_rename(file_t *from, file_t *to);
 
 void dedup_load(const char *dir);
 void dedup_save();
+
+int dedup_hash_file(const char *name, unsigned char *md5);
+void dedup_add(unsigned char *md5, const char *filename);
+int dedup_db_has(unsigned char *md5);
+void dedup_init_db(void);
