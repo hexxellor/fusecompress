2f5d605b880fbb7b3fd4ffb51dc847fbe11db11c
ulrich.hecht <ulrich.hecht@36aca67c-b753-0410-8cf6-e3b166235498>
2011-11-08 14:22:17 +0000

    uncompressible files: added missing dot
    Signed-off-by: Ulrich Hecht <uli@suse.de>

--- globals.c
+++ globals.c
@@ -60,7 +60,7 @@ compressor_t *compressors[5] = {
 char *incompressible[] = {
     ".mp3", ".ogg",
     ".avi", ".mov", ".mpg", ".mp4", ".mkv", ".asf", ".flv",
-    ".gz", ".bz2", ".zip", ".tgz", ".lzo", ".lzma", ".rar", ".ace", "7z", ".lha",
+    ".gz", ".bz2", ".zip", ".tgz", ".lzo", ".lzma", ".rar", ".ace", ".7z", ".lha",
     ".lzh", ".chm", ".lrz", ".xz",
     ".jpg", ".png", ".gif",
     ".rpm", ".deb",
