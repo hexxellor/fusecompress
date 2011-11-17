c346ef51d2bc01587e4e136e75f6d34f41a7a1f4
ulrich.hecht <ulrich.hecht@36aca67c-b753-0410-8cf6-e3b166235498>
2011-11-17 13:07:30 +0000

    fsck: remove trailing garbage check
    It creates WAY too many false positives; the assumption that decompressors
    actually read all the way to the end of the compressed text does not seem
    to hold. Since it is neither a common nor a serious issue, I just removed
    the check.
    
    Signed-off-by: Ulrich Hecht <uli@suse.de>

--- trunk/test/fsck.sh
+++ trunk/test/fsck.sh
@@ -4,13 +4,10 @@ mkdir test
 dd if=/dev/urandom of=test/file bs=10k count=1
 cp test/file test/file2
 cp test/file test/file3
-cp test/file test/file4
 chmod 000 test/file3	# see if we can open a no permissions file
 fusermount -u test
 usleep 500000
 dd if=/dev/null of=test/file bs=1k seek=5	# break test/file
-dd if=/dev/urandom bs=1k count=1 >>test/file4	# create valid file with garbage at the end
-chmod 400 test/file4	# see if we can trunc a read-only file
 # check if the files behave as expected
 ../fusecompress -o detach test
 cat test/file >/dev/null && false || true
@@ -28,17 +25,11 @@ set +e
 ../fsck.fusecompress -d test
 ret=$?
 set -e
-test $ret == 4
+test $ret == 1
 test ! -e test/file	# test/file is broken, it should have been removed
-set +e
-../fsck.fusecompress -p test
-ret=$?
-set -e
-test $ret == 1	# all errors should have been fixed
 ../fusecompress -o detach test
 chmod 644 test/file3
 cmp test/file2 test/file3
-cmp test/file3 test/file4
 fusermount -u test
 usleep 500000
 rm -fr test
